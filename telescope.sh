e53efacbfeebf020447d50fa828b7464363458f9
Dustin Sallings <dustin@spy.net>
2009-11-01 01:27:52 -0700

    Have the revision control systems do all the work.

--- telescope.sh
+++ telescope.sh
@@ -1,58 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
-##############################################################################
-#
-# usage: logRevisionsOfDir $subdir $template $outputFile
-#
-#
-logRevisionsOfDir()
-{
-	subdir=$1
-	template=$2
-	outputFile=$3
+hg convert --filemap convert-file-map onnv-gate zfs-converted-hg
 
-    echo "# will log ${subdir}"
-    echo "# will log ${subdir}" >>  ${outputFile}
-    hg log ${subdir} --template ${template} >> ${outputFile}
-}
+mkdir zfs-converted-git
+cd zfs-converted-git
+git init
+../fast-export/hg-fast-export.sh -r ../zfs-converted-hg
+git reset --hard
+git gc
 
-
-##############################################################################
-#
-#   MAIN
-#
-#
-
-HGSOURCEROOT="/Volumes/SolarisSource/onnv-gate/"
-echo "HGSOURCEROOT: $HGSOURCEROOT"
-TEMPLATE="{rev}:::{node}:::{desc|firstline}\n"
-OUTPUTPATH="/tmp/log.txt"
-(
-cd $HGSOURCEROOT
-
-echo "# revisions" > $OUTPUTPATH
-
-logRevisionsOfDir "usr/src/uts/common/fs/zfs/" $TEMPLATE $OUTPUTPATH
-logRevisionsOfDir "usr/src/common/zfs/" $TEMPLATE $OUTPUTPATH
-logRevisionsOfDir "usr/src/uts/common/sys/fs/zfs.h" $TEMPLATE $OUTPUTPATH
-logRevisionsOfDir "usr/src/uts/common/sys/fm/fs/zfs.h" $TEMPLATE $OUTPUTPATH
-logRevisionsOfDir "usr/src/lib/libzfs/common/" $TEMPLATE $OUTPUTPATH
-logRevisionsOfDir "usr/src/cmd/zfs/" $TEMPLATE $OUTPUTPATH
-logRevisionsOfDir "usr/src/cmd/zpool/" $TEMPLATE $OUTPUTPATH
-
-# this is -sort of- part of zfs (but will lead to many non-important changes)
-#logRevisionsOfDir "$HGSOURCEROOT/usr/src/uts/common/sys/" $TEMPLATE $OUTPUTPATH
-
-)
-
-# clean and build parselog
-xcodebuild -configuration Release clean
-xcodebuild -configuration Release build
-
-# call parselog
-build/Release/parselog /tmp/log.txt > "$HGSOURCEROOT/transmogrify.sh"
-
-echo "transmogrify.sh" > "$HGSOURCEROOT/.gitignore"
-
-# make executable
-chmod u+x "$HGSOURCEROOT/transmogrify.sh"
+rm -rf zfs-converted-hg
